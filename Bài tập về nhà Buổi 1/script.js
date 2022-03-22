0416a97b3a4e1f9f15ef08938f96ce07de3dd25c
Sondohoang0709 <96523863+Sondohoang0709@users.noreply.github.com>
2022-03-22 17:27:48 +0700

    Add files via upload

---
+++ Bài tập về nhà Buổi 1/script.js
@@ -0,0 +1,14 @@
+
+let text = "abcde";
+const myArr=text.split("");
+const newArr= myArr.reverse();
+//console.log(newArr)
+
+let txt= "";
+newArr.forEach(myFunction);
+
+ function myFunction(value)  {
+  txt +=  value + ""; 
+}
+console.log(txt)
+
